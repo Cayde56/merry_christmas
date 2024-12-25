b9c04f0c4e569dcd637b4101976d92ec6ded4055
Cayde56 <manueladrianabre@gmail.com>
2024-12-24 19:22:04 -0600

    Initial commit

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
-import router from "./routers/routers.js";
+import router from "./routers/routers.js"
 import express from "express";
 import cors from "cors";
+import process from "process";
 import { join, dirname } from "path";
 import { fileURLToPath } from "url";
+import dotenv from 'dotenv'
 
+dotenv.config()
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 const __dirname = dirname(fileURLToPath(import.meta.url))
 
 app.use(cors());
